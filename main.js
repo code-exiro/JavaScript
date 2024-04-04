69993278666462fb9e01ff6c93347dee4f996f61
erizo <nandezemir@gmail.com>
2024-04-04 12:58:26 -0600

    clase 3, paso por valor y referencia

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-//Clase 2 variables
+//Clase 3, tipos de datos
 
-let caja = 'oso'
-console.log(caja);
+//Primitivos - inmutable - 7
 
-const pi = 3.1416
-console.log(pi);
+let numero = 23
+numero = numero + 12
+console.log(numero)
+
+let verdad = true 
+verdad = false 
+console.log(verdad)
+
+//Complejos - mutable - 3
+
+let usuario = {nombre: 'Emir', edad: 20}
+usuario.edad = 21
+console.log(usuario)
+
+let frutas = ['manzana', 'pera']
+frutas[0] = 'sandia'
+console.log(frutas)
+
+//------------------------
+
+function cambiarNombre (objeto){
+    objeto.nombre = 'nuevo nombre'
+}
+
+let persona = {nombre: 'Antonio'}
+cambiarNombre(persona)
+
+console.log(persona)
