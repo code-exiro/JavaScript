217e5b2e8659e5a6e067cd86a360968ae7e5c3ca
erizo <nandezemir@gmail.com>
2024-04-07 23:25:21 -0600

    Clase 11, Anatomia de una funcion

--- main.js
+++ main.js
@@ -29,3 +29,42 @@ Follow me for coding adventures!.
 console.log(personalizedBio)
 
 console.log('-------------------------------------------------------')
+
+//Clase 11 - Anatomia de una funcion 
+
+function suma (a, b){     //1.Palabra clave(function) 2.Nombre de nuestra funcion(suma en este caso)   
+    return a + b          //3.Los parametros o argumentos que va recibir 4.Cuerpo(return) lo que va a regresar nuestra funcion
+}
+console.log(suma(299, 1))   //5.Llamdo de la funcion 
+
+function calcularDescuento (precio, porcentajeDescuento){
+    const descuento = (precio * porcentajeDescuento) / 100
+    const precioConDescuento = precio - descuento
+
+    return precioConDescuento
+}
+
+const precio = 1299
+const porcentajeDescuento = 25
+const precioFinal = calcularDescuento(precio, porcentajeDescuento)
+
+console.log('Precio original $' + precio)
+console.log('Descuento ' + porcentajeDescuento + '%')
+console.log('Precio con descuento: $' + precioFinal)
+
+// function calculateDiscount (price, discountRate){
+//     const discount = (price * discountRate) / 100
+//     const priceWithDiscount = price - discount
+
+//     return priceWithDiscount
+// }
+
+// const price = 100
+// const discountRate = 20
+// const finalPrice = calculateDiscount(price, discountRate)
+
+// console.log('Original Price $' + price)
+// console.log('Discount ' + discountRate + '%')
+// console.log('Price with discount: $' + finalPrice)
+
+console.log('-------------------------------------------------------------------')
