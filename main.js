9970deb8ad7923c3c4401f733a989b10bb89c9f8
erizo <nandezemir@gmail.com>
2024-04-10 23:57:56 -0600

    Clase 14, this

--- main.js
+++ main.js
@@ -32,6 +32,15 @@ console.log(finalResult)
 //5.Manipulacion del DOM
 //6.Obtener la hora actual
 
+const valor = 10;               
+function sumarValor (a) {               //depende de datos externos('valor')
+    return a + valor;
+}
+
+function establecerNombre (usuario) {   //modifica un estado externo
+    usuario.nombre = "Juan";
+}
+  
 function sum (a ,b) {
     console.log('A: ', a)       //tiene efecto secundario(como registrar un log)
     return a + b
@@ -46,3 +55,35 @@ function sumaConEfecto () {     //modifica la variable global total
 
 console.log('--------------------------------------------')
 
+//Clase 14 - This ---> concepto fundamental 
+
+//Enlace implicito o implicit binding
+const house = {         //Se crea el objeto house con dos propiedades
+    dogName: 'sheldon',  //La primer propiedad es dogName que es una cadena de texto sheldon
+    dogGreeting: function() {  //La segunda propiedad es dogGreeting que es una funcion 
+        console.log(`Hi, my name is ${this.dogName}`)
+    }
+}
+house.dogGreeting() //Aqui estoy llamando a la propiedad 'dogGreeting' de mi objeto 'house'
+
+//Enlace explicito o explicit binding
+
+function dogGreeting() {    //Definiendo dogGreeting en el ambito global
+    console.log(`no me roben los huevos gritaba ${this.dogName}`)
+}
+
+//En el caso de que NO tuviera dogGreeting como global podria llamar a dogGreeting atraves del objeto house
+//ejemplo: 'house.dogGreeting.call(newHouse);'
+
+const newHouse = {
+    dogName: 'princesa'
+}
+
+dogGreeting.call(newHouse)  //El metodo .call permite llamar a una funcion con valor 'this' especifico y argumentos individuales 
+
+function newDogGreeting(owner,address){console.log(`Hi, I'm ${this.dogName} and I live with ${owner} on ${address}`)}
+const owner='Juan'
+const address='gothams'
+newDogGreeting.call(newHouse,owner,address)
+
+console.log('------------------------------------------')
