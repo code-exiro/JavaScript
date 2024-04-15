09c4b3e7da191b59ed3f719c6023d206c682a35a
erizo <nandezemir@gmail.com>
2024-04-14 22:53:45 -0600

    Clase 17, funciones constructoras

--- main.js
+++ main.js
@@ -42,3 +42,28 @@ const fictionalCharacter = {
 
 fictionalCharacter.mensajeConFuncionTradiccional('un gran poder blablabla muere')
 fictionalCharacter.mensajeConArrowFunction('un gran poder blablabla muere')
+
+console.log('------------------------------------------------------------')
+
+//Clase 17 - Funciones constructoras
+
+//implicaciones de Duplicar codigo implica 
+//1.mantenimiento dificil
+//2.Aumento de la complejidad 
+//3.Mayor probabilidad de errores
+//4.Dificultad de escalar
+//5.Tiempo y recursos
+//6.Violacion de principios de diseno(DRY)
+//7.Dificultad en la identificacion de errores
+
+function Rocket(name) {
+    this.name = name
+    this.lauchMessage = function() {
+        console.log('flama')
+    }
+}
+
+const falcon9Rocket = new Rocket('Falcon 9')
+const falconHeavyRocket = new Rocket('Falcon Heavy')
+console.log(falcon9Rocket.name)
+console.log(falcon9Rocket.lauchMessage())
