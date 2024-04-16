66423946bef749dc374036a1ff80da4a794adbcc
erizo <nandezemir@gmail.com>
2024-04-16 07:38:11 -0600

    Clase 18, prototype

--- main.js
+++ main.js
@@ -45,25 +45,54 @@ fictionalCharacter.mensajeConArrowFunction('un gran poder blablabla muere')
 
 console.log('------------------------------------------------------------')
 
-//Clase 17 - Funciones constructoras
+//Clase 17 - Funciones constructoras ---> es simplemente una funcion regular que se utiliza para construir objetos.
 
-//implicaciones de Duplicar codigo implica 
-//1.mantenimiento dificil
-//2.Aumento de la complejidad 
-//3.Mayor probabilidad de errores
-//4.Dificultad de escalar
-//5.Tiempo y recursos
-//6.Violacion de principios de diseno(DRY)
-//7.Dificultad en la identificacion de errores
+//JavaScript utiliza un paradigma basado en prototipos en lugar de clases basadas en la herencia clasica.
 
-function Rocket(name) {
-    this.name = name
-    this.lauchMessage = function() {
-        console.log('flama')
+function Persona(nombre, edad) {  //Las funciones constructoras comienzan con mayuscula(por convencion)
+    this.nombre = nombre;  //'this' se refiere al nuevo objeto que se crea cuando
+    this.edad = edad;      //se invoca la funcion constructora con el operador 'new'
+
+    this.saludar = function() {   //metodo asignado al nuevo objeto  este se sigue refiriendo al objeto desde el cual se invoco el metodo 
+        console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} anios`)
     }
 }
 
-const falcon9Rocket = new Rocket('Falcon 9')
-const falconHeavyRocket = new Rocket('Falcon Heavy')
-console.log(falcon9Rocket.name)
-console.log(falcon9Rocket.lauchMessage())
+
+const persona1 = new Persona("Carlos", 50)   //Creacion de una instancia    
+const persona2 = new Persona('Juan', 23)     //al utilizar 'new' se crea una instancia del objeto 'persona' 
+persona1.saludar()
+persona2.saludar()   //persona2 es ahora un objeto con las propiedades 'nombre' y 'edad' configuradas y un metodo saludar 
+
+//En este ejemplo cada instancia de 'persona' tiene su propio conjunto de propiedades 'nombre' y 'edad' asi como un metodo 'saludar'
+//Definir metodos directamente dentro de la funcion constructora no es eficiente en terminos de 
+//memoria porque cada instancia tiene su propia copia del metodo como se puede ver en el ejemplo (saludar)
+
+console.log('-----------------------------------------------------------')
+
+//Prototipos(prototype)
+
+function Coche(modelo, anio) { //Primero definimos la funcion constructora 'Coche' que inicializa nuevas instancias
+    this.modelo = modelo       //con algunas propiedades especificas como el 'nodelo' y el 'anio' 
+    this.anio = anio
+    this.kilometraje = 0
+}
+
+Coche.prototype.conducir = function(distancia) {  //Método para simular conducir el coche y aumentar el kilometraje
+    this.kilometraje += distancia;
+    console.log(`Condujiste ${distancia} km en el ${this.modelo} y ahora el kilometraje es ${this.kilometraje} km.`);
+};
+
+Coche.prototype.informacion = function() {      //Método para obtener información del coche
+    console.log(`Modelo: ${this.modelo}, Año: ${this.anio}, Kilometraje: ${this.kilometraje}`);
+};
+
+const lambo = new Coche('lambo', 2024)
+const ferrari = new Coche('ferrari', 2023)
+
+lambo.conducir(1500)
+ferrari.conducir(500)
+lambo.conducir(1000)
+ferrari.conducir(2000)
+lambo.informacion()
+ferrari.informacion()
