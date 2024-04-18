b06383fcd50ee0ec3cdb5d197236fdcc6d542a6b
erizo <nandezemir@gmail.com>
2024-04-18 13:40:41 -0600

    Clase 22, Operadores logicos

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 //Clase 21 - Operadores de comparacion 
 
-
 // ==    ---> Igualdad
 console.log(1 == 1); // true
 console.log('1' == 1); // true, porque '1' se convierte a 1
 
 // ===   ---> Estrictamente igual
-console.log(1 != 2); // true
-console.log(1 != '1'); // false, porque '1' se convierte a 1
+console.log(1 === 1); // true
+console.log('1' === 1); // false, porque los tipos son diferentes
 
 // !=    ---> No igual  
 console.log(1 != 2); // true
@@ -33,16 +32,37 @@ console.log(2 >= 3); // false
 console.log(2 <= 3); // true
 console.log(3 <= 2); // false
 
+//validaciones en la consola del navegador ya que esta entiende JS
 const a = 10;
 const b = 20;
 const c = "10";
 
-//validaciones en la consola del navegador ya que esta entiende JS
+a == b;    //false
+a === c;   //false    
+a != b;    //true
+a !== c;   //true    
+a > b;     //false
+a <= b;    //true
+a > c;     //false
 
-a == b;     //false
-a === c;    //false    
-a != b;     //true
-a !== c;    //true    
-a > b;      //false
-a <= b;     //true
-a > c       //false
+console.log('------------------------------------------------------------')
+
+//Clase 22 - Operadores logicos 
+
+// &&    ---> AND true si ambos son verdaderos, si uno de los operandos es falso es false 
+console.log(true && true); // true
+console.log(true && false); // false
+
+// ||    ---> OR true si al menos uno es verdadero, si ambos son falsos es false
+console.log(true || false); // true
+console.log(false || false); // false
+
+// !     ---> NOT invierte el valor booleano de una condicion  
+console.log(!true); // false
+console.log(!false); // true
+
+a == b && a === c;   //false
+a != b || a === c;   //true
+!(a === c)           //true
+
+console.log('------------------------------------------------------')
